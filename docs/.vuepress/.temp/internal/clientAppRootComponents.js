b1cc551afa6852092a1c1cd5960a26d0707ff787
Ananthakrishnan <ananthakrishnan.b7@gmail.com>
2022-05-07 14:03:57 +0530

    Git ingore .vuepress

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/home/ananthakrishnan/baji/projects/vue3-google-login/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- docs/.vuepress/.temp/internal/pagesComponents.js
+++
@@ -1,8 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const pagesComponents = {
-  // path: /
-  "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"/home/ananthakrishnan/baji/projects/vue3-google-login/docs/.vuepress/.temp/pages/index.html.vue")),
-  // path: /404.html
-  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"/home/ananthakrishnan/baji/projects/vue3-google-login/docs/.vuepress/.temp/pages/404.html.vue")),
-}
