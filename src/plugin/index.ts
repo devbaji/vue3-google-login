bf7ba133f03fa7db3f1039f08a9d924669dcfca2
Ananthakrishnan <ananthakrishnan.b7@gmail.com>
2022-05-14 12:03:27 +0530

    useLibraryLoaded composible function added

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,4 +1,4 @@
-import { App } from "vue";
+import { App, Ref, ref, watch } from "vue";
 import * as types from "./types";
 import * as utils from "./utils";
 import state, { setState } from "./state";
@@ -7,6 +7,17 @@ import GoogleLogin from "./GoogleLogin.vue";
 export const gLoginPopup = utils.openPopup;
 
 export const decodeCredential = utils.parseJwt;
+
+export const useLibraryLoaded = (): Ref<boolean> => {
+  const loaded = ref(false);
+  watch(
+    () => state.apiLoaded,
+    (n) => {
+      loaded.value = n;
+    }
+  );
+  return loaded;
+};
 
 export default {
   install: (app: App, options: types.options) => {
