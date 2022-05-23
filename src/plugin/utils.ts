1c08b4aabdb6d754b6253b1342fb64812b9f4492
Ananthakrishnan <ananthakrishnan.b7@gmail.com>
2022-05-23 22:40:29 +0530

    fix: remove uuidv4 and buttonId, resolve client-id undefined issue

--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -8,14 +8,6 @@ declare global {
   interface Window extends types._Window {}
 }
 
-export const uuidv4 = (): string => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-};
-
 /**
  * For retriving the JWT payload from the credential
  * @param token JWT credential string
@@ -132,6 +124,7 @@ export const onMount = (
     );
     options.prompt &&
       googleOneTap({
+        clientId: options.clientId,
         callback: options.callback as callbackTypes.CredentialCallback,
         error: options.error,
         autoLogin: options.autoLogin
